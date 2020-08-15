4e7f7458c05c8fc4818fa5155c3f8dc6de2a6a1c
TLNBS2405 <33620644+TLNBS2405@users.noreply.github.com>
2020-08-15 21:13:10 +0200

    Add dialog to choose path

--- src/ImgurClient.py
+++ src/ImgurClient.py
@@ -1,32 +1,32 @@
 from imgur_python import Imgur
 import glob
 
+
 class ImgurClient:
 
     def __init__(self, client_id, access_token):
         self.ic = Imgur({'client_id': client_id, 'access_token': access_token})
 
     def create_album(self, name: str):
-
-        album =  self.ic.album_create([], name, '', 'public')
-        print(album)
+        album = self.ic.album_create([], name, '', 'public')
         album_id = album['response']['data']['id']
         return album_id
 
-    def upload_gif(self, path: str, title: str = None, desc : str = None, album_hash: str = None):
-
-        response = self.ic.image_upload(path, title, desc, album=album_hash)
+    def upload_gif(self, path: str, title: str = None, desc: str = None, album_hash: str = None):
+        print("uploading " + path)
+        response = self.ic.image_upload(filename=path, title=title, description=desc, album=album_hash)
         return response
 
     def upload_folder(self, path: str):
-        all_status = []
-        last_part = path.split("/")[-1]
+        error_status = {}
+        album_name = path.split("/")[-1]
 
-        album_id = self.create_album(last_part)
+        album_id = self.create_album(album_name)
 
-        all_files = glob.glob(path+"\\*")
+        all_files = glob.glob(path + "\\*")
         for file in all_files:
-            status = self.upload_gif(file,album_hash=album_id)["status"]
-            all_status.append(status)
-        return all_status
-
+            file_name = file.split("\\")[-1]
+            status = self.upload_gif(path=file, title=file_name, album_hash=album_id)["status"]
+            if status != 200:
+                error_status[file] = status
+        return error_status
--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 from src import ImgurClient
 import json
+import tkinter as tk
+from tkinter import filedialog
 
 with open("config.json") as config_json:
     config_data = json.load(config_json)
 
 ic = ImgurClient.ImgurClient(config_data["client_ID"], config_data["access_token"])
+tk.Tk().withdraw()
+dirname = filedialog.askdirectory(initialdir="/", title='Please select a directory', mustexist=True)
 
+if dirname:
+    error = ic.upload_folder(dirname)
+    if error:
+        print("Error:", error)
+print("finished")
